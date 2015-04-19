7f45db477bd31ae7838cf1cd1c5b7370de11f94e
Corey Reichle <corey.reichle@gmail.com>
2015-04-18 21:04:46 -0400

    Added the make_dirs.sh script.  This will generated the standard directory layout.

---
+++ make_dirs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Script to create ansible playbook directories
+
+# define your roles here
+roles=(common webservers db desktop)
+directories=(tasks handlers templates files vars defaults meta)
+
+# create playbook
+mkdir global_vars
+mkdir roles
+mkdir library
+mkdir filer_plugins
+mkdir host_vars
+mkdir production
+mkdir stage
+
+for i in ${roles[@]}; do
+  for j in ${directories[@]}; do
+    mkdir -p roles/${i}/${j}
+  done
+done
+
+for i in ${roles[@]}; do
+  touch roles/$i/tasks/main.yml
+  touch roles/$i/handlers/main.yml
+  touch roles/$i/vars/main.yml
+  touch roles/$i/meta/main.yml
+done
+
+touch site.yml
+touch hosts
+
+exit 0
