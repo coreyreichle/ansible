2171277b19a14fb27297f141d4b368512f49a748
Corey Reichle <corey.reichle@gmail.com>
2015-04-20 14:50:20 -0400

    Added workstation role, and started on webserver role

---
+++ roles/workstations/files/backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# No, we're not going to setup any backup scheme here.  It's just an example.
+
+echo "This is our backup cronjob, which should run at midnight, every day, per the play task."
