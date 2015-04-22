290e80cf0d26b2c2d815d8fd50f212e74017a402
Corey Reichle <corey.reichle@gmail.com>
2015-04-22 10:51:55 -0400

    Added a neat trick to *copy* the localuser running ansible over to the remote machine, with authorized key

---
+++ build_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in web01 web02 web db01 work01; do sudo lxc-create -n $i -t ubuntu; done
+
+for i in web01 web02 web db01 work01; do sudo lxc-start -n $i; done; sudo lxc-ls --fancy
+
+sleep 15;
+
+for i in `sudo lxc-ls`; do sudo lxc-attach -n $i -- /usr/bin/apt-get -y install python ; done
+
+ansible -i ./hosts all -m ping -u ubuntu -k
+
+ansible-playbook site.yml -i ./hosts  -k -K
