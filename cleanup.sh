7b9dd69cedab02d2a78c0468ecad9825dfa079bf
Corey Reichle <corey.reichle@gmail.com>
2015-04-28 11:17:19 -0400

    Tweak to the cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in db01 web web01 web02 work01; do sudo lxc-stop -n $i; ssh-keygen -R $i; sudo lxc-destroy -n $i; done
+for i in db01 web web01 web02 work01; do sudo lxc-stop -n $i; ssh-keygen -R $i.lxc; sudo lxc-destroy -n $i; done
